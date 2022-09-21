ca4b4b34557d2398d47904d20d96f3839ce8f1e7
QuangNV <quangbeo199x@gmail.com>
2022-09-21 14:41:17 +0700

    update AddImageToStory

--- BTogether.Web/Areas/ConfigPage/Pages/AddImageToStory.cshtml.cs
+++ BTogether.Web/Areas/ConfigPage/Pages/AddImageToStory.cshtml.cs
@@ -29,33 +29,46 @@ namespace BTogether.Web.Areas.ConfigPage.Pages
         [BindProperty]
         public IFormFile FileUpload { get; set; }
 
+        public string ReturnUrl { get; set; }
+
         public Story StoryView { get; set; }
 
         public string UserId { get; set; }
 
         public IEnumerable<ImageMemory> ImageMemories { get; set; }
 
+        [BindProperty]
         public InputModel Input { get; set; }
 
         public class InputModel
         {
+            public string Url { get; set; }
+
+            public int StoryId { get; set; }
+
             public string Description { get; set; }
         }
 
-        public async Task<IActionResult> OnGetAsync(int storyId)
+        public async Task<IActionResult> OnGetAsync(int storyId, string returnUrl = null)
         {
             UserId = _userManager.GetUserId(User);
             StoryView = await _storyService.GetByIdAsync(storyId);
             ImageMemories = await _imageMemoryService.GetImageMemoriesByStoryIdAsync(storyId);
-
+            Input = new InputModel
+            {
+                StoryId = storyId
+            };
+            returnUrl ??= Url.Content("~/ConfigPage/AddImageToStory?storyId="+StoryView.Id);
+            ReturnUrl = returnUrl;
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            returnUrl  ??= Url.Content("~/ConfigPage/AddImageToStory?storyId=" + Input.StoryId);
             if (FileUpload != null)
             {
-                string folderPath = _environment.WebRootPath +"\\"+ UserId;
+                string folderPath = _environment.WebRootPath +"\\images\\"+ _userManager.GetUserId(User);
                 if (Directory.Exists(folderPath))
                 {
                     var file = Path.Combine(_environment.WebRootPath, folderPath, FileUpload.FileName);
@@ -77,14 +90,43 @@ namespace BTogether.Web.Areas.ConfigPage.Pages
 
             var imageMem = new ImageMemory
             {
-                StoryId = StoryView.Id,
+                StoryId = Input.StoryId,
                 Url = FileUpload.FileName,
                 Description = Input.Description
             };
 
             await _imageMemoryService.AddAsync(imageMem);
 
-            return RedirectToPage();
+            return LocalRedirect(returnUrl);
+        }
+
+        public async Task<IActionResult> OnGetEditAsync(int id)
+        {
+            var image = await _imageMemoryService.GetByIdAsync(id);
+            return new JsonResult(image);
+        }
+
+        public async Task<IActionResult> OnPostEditAsync(int id, string returnUrl = null)
+        {
+            var image = await _imageMemoryService.GetByIdAsync(id);
+            returnUrl ??= Url.Content("~/ConfigPage/AddImageToStory?storyId=" + image.StoryId);
+            image.Description = Input.Description;
+            await _imageMemoryService.UpdateAsync(image);
+            return LocalRedirect(returnUrl);
+        }
+
+        public async Task<IActionResult> OnGetDeleteAsync(int id)
+        {
+            var image = await _imageMemoryService.GetByIdAsync(id);
+            return new JsonResult(image);
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id, string returnUrl = null)
+        {
+            var image = await _imageMemoryService.GetByIdAsync(id);
+            returnUrl ??= Url.Content("~/ConfigPage/AddImageToStory?storyId=" + image.StoryId);
+            await _imageMemoryService.DeleteAsync(image);
+            return LocalRedirect(returnUrl);
         }
     }
 }
